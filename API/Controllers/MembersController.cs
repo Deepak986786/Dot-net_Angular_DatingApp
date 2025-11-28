4708e32f6accea5a7915d25cef3bc2d95abfd17f
Deepak <deepaksinghcse1822@gmail.com>
2025-11-29 04:54:00 +0530

    initial commit

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public MembersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AppUser>>> GetMembers()
+        {
+            var members = await _context.Users.ToListAsync();
+            return members;
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetMemberById(string Id)
+        {
+            var member = await _context.Users.FirstOrDefaultAsync(x => x.Id == Id);
+            return Ok(member);
+        }
+    }
+}
